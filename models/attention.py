e39fbc98dd6358d49e37216bc5a037ea91752b4f
sunrenxiang <100749310+sunrenxiang@users.noreply.github.com>
2022-12-08 21:02:04 +0800

    Update attention.py

--- models/attention.py
+++ models/attention.py
@@ -4,69 +4,55 @@ from torch import nn
 from torch.nn import init
 
 class ChannelAttention(nn.Module):
-    def __init__(self,channel,reduction=16):
-        super().__init__()
-        self.maxpool=nn.AdaptiveMaxPool2d(1)
-        self.avgpool=nn.AdaptiveAvgPool2d(1)
-        self.se=nn.Sequential(
-            nn.Conv2d(channel,channel//reduction,1,bias=False),
-            nn.ReLU(),
-            nn.Conv2d(channel//reduction,channel,1,bias=False)
-        )
-        self.sigmoid=nn.Sigmoid()
-    
-    def forward(self, x) :
-        max_result=self.maxpool(x)
-        avg_result=self.avgpool(x)
-        max_out=self.se(max_result)
-        avg_out=self.se(avg_result)
-        output=self.sigmoid(max_out+avg_out)
-        return output
+    def __init__(self, channels):
+        super(ChannelAttention, self).__init__()
+
+        self.avg = nn.AdaptiveAvgPool2d(1)
+        self.max = nn.AdaptiveMaxPool2d(1)
+
+        self.conv1 = nn.Conv2d(channels, channels, kernel_size=1, padding=0)
+        self.conv2 = nn.Conv2d(channels, channels, kernel_size=1, padding=0)
+
+        self.relu = nn.ReLU(inplace=True)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        ori = x
+        out1 = self.conv2(self.relu(self.conv1(self.avg(x))))
+        out2 = self.conv2(self.relu(self.conv1(self.max(x))))
+        out = self.sigmoid(out1 + out2)
+        out = ori * out
+        return out
 
 
 class SpatialAttention(nn.Module):
-    def __init__(self,kernel_size=7):
-        super().__init__()
-        self.conv=nn.Conv2d(2,1,kernel_size=kernel_size,padding=kernel_size//2)
-        self.sigmoid=nn.Sigmoid()
-    
-    def forward(self, x) :
-        max_result,_=torch.max(x,dim=1,keepdim=True)
-        avg_result=torch.mean(x,dim=1,keepdim=True)
-        result=torch.cat([max_result,avg_result],1)
-        output=self.conv(result)
-        output=self.sigmoid(output)
-        return output
+    def __init__(self, kernel_size=3):
+        super(SpatialAttention, self).__init__()
+
+        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
+        padding = 3 if kernel_size == 7 else 1
+
+        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
+        self.conv2 = nn.Conv2d(2, 1, kernel_size, padding=2, dilation=2, bias=False)
+        self.conv3 = nn.Conv2d(2, 1, kernel_size, padding=3, dilation=3, bias=False)
+        self.conv4 = nn.Conv2d(2, 1, kernel_size, padding=4, dilation=4, bias=False)
+        self.conv = nn.Conv2d(4, 1, kernel_size=1, padding=0, bias=False)
+        self.sigmoid = nn.Sigmoid()
 
 
 class AttentionBlock(nn.Module):
 
     def __init__(self, channel=512,reduction=16,kernel_size=49):
         super().__init__()
-        self.ca=ChannelAttention(channel=channel,reduction=reduction)
-        self.sa=SpatialAttention(kernel_size=kernel_size)
 
-
-    def init_weights(self):
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                init.kaiming_normal_(m.weight, mode='fan_out')
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
-            elif isinstance(m, nn.BatchNorm2d):
-                init.constant_(m.weight, 1)
-                init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                init.normal_(m.weight, std=0.001)
-                if m.bias is not None:
-                    init.constant_(m.bias, 0)
+        self.channel = ChannelAttention(channel=channel,reduction=reduction)
+        self.spatial = SpatialAttention(kernel_size=kernel_size)
 
     def forward(self, x):
-        b, c, _, _ = x.size()
-        residual=x
-        out=x*self.ca(x)
-        out=out*self.sa(out)
-        return out+residual
+        ori = x
+        out = x * self.channel(x)
+        out = out * self.spatial(out)
+        return out + ori
 
 if __name__ == '__main__':
     input=torch.randn(50,512,7,7)
